7a0b560110317e973b48b4d5868bf924272aeca9
therealric <therealricvannemerwe@icloud.com>
2026-02-19 18:40:57 +0200

    Refactor Fibonacci Sequence

--- Tasks/task2.php
+++ Tasks/task2.php
@@ -7,17 +7,16 @@ function fibonacci()
     $number2 = 1;
     $result = 0;
 
-    while ($result <= 34) {
-        echo $result . ", ";
+    echo $number1 . ", ";
+    echo $number2;
 
-        if ($result === 0) {
-            echo $number2 . ", ";
-        }
-
+    while ($result < 34) {
         $result = $number1 + $number2;
 
         $number1 = $number2;
         $number2 = $result;
+
+        echo  ", " . $result;
     }
 }
 
